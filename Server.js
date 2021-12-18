a108baa2af8b8cfe2f7073672fef50e36ec94de2
Naor Bar-lev <naorbarlev@gmail.com>
2021-12-18 23:45:56 +0200

    working version with comments

--- Server.js
+++ Server.js
@@ -1,9 +1,11 @@
 // Import dependencies
 const prompt = require("prompt-sync")();
-
 const express = require("express");
+const { emitKeypressEvents } = require("readline");
 const app = express();
 const http = require("http").createServer(app);
+
+//corsטיפול בשגיאות שנזרקות מה
 const socketIO = require("socket.io")(http, {
   cors: {
     origin: "*",
@@ -13,12 +15,13 @@ const socketIO = require("socket.io")(http, {
 const port = 4001;
 
 let altitude, HIS, ADI;
-
+//הקמת שרת והאזנה לפורט 4001
 http.listen(port, function () {
-  console.log("server started...listening at 4001  ");
+  console.log("Server is UP...");
   socketIO.on("connection", function (socket) {
-    console.log("user connected: " + socket.id);
+    // קלט גובה מהמשתמש בטווח של 0 ל 3000
     altitude = prompt("Please Enter Altitude: ");
+    //בדיקת תקינות הקלט
     while (
       altitude > 3000 ||
       altitude < 0 ||
@@ -29,8 +32,9 @@ http.listen(port, function () {
       console.log("try again...");
       altitude = prompt("Please Enter Altitude: ");
     }
-    //ask for HIS if not between 0 to 360
+    //קלט כיוון בטווח של 0 ל360
     HIS = prompt("Please Enter HIS: ");
+    //בדיקת תקינות קלט
     while (
       HIS > 360 ||
       HIS < 0 ||
@@ -41,8 +45,9 @@ http.listen(port, function () {
       console.log("try again...");
       HIS = prompt("Please Enter HIS: ");
     }
-    //ask for altitude if not between -100 to 100
+    //קלט זווית האופק
     ADI = prompt("Please Enter ADI: ");
+    //בדיקת תקינות הקלט
     while (
       ADI < -100 ||
       ADI > 100 ||
@@ -54,22 +59,11 @@ http.listen(port, function () {
       ADI = prompt("Please Enter ADI: ");
     }
 
+    //שמירת הערכים מהקונסול לתוך אובייקט שישלח ללקוח כפורמט של מפתח-ערך
     let dataObj = { altitude: altitude, his: HIS, adi: ADI };
 
-    // let dataArray = [altitude,HIS,ADI];
-
     //להוסיף כאן לחיצה לשליחה
-    console.log("Sending values => " + dataObj);
+    console.log("Sending values...");
     socket.emit("SEND_OBJECT", dataObj);
-    // socket.json.send(dataObj);
-
-    socketIO.on("disconnect", () => {
-      console.log("Client disconnected");
-    });
   });
 });
-
-// console.log('Press any key to exit');
-// process.stdin.setRawMode(true);
-// process.stdin.resume();
-// process.stdin.on('data', process.exit.bind(process, 0));
